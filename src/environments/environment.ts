9dc628d08e78bc79a7282808c2b1323e0826d645
nosferatum2 <artworkvo@gmail.com>
2024-12-12 10:51:06 +0200

    removed dotenv and used default angular env approach

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+
+  WEATHER_URL_API: "https://api.openweathermap.org/data/2.5/weather",
+  GEOCODING_URL_API: "https://api.openweathermap.org/geo/1.0/direct",
+
+  APP_ID: "d9a0985de8cb79a9c27c212214fe390f",
+
+};

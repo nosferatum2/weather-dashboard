ec5ee8106a72d07b180835658e785a6eca42c58e
nosferatum2 <artworkvo@gmail.com>
2024-12-17 15:40:39 +0200

    List of cities added to localStorage

--- src/app/pages/weather-dashboard/weather-dashboard.component.ts
+++ src/app/pages/weather-dashboard/weather-dashboard.component.ts
@@ -1,12 +1,13 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { GeocodingAPIService } from '../../shared/services/geocoding-api/geocoding-api.service';
 import { WeatherApiService } from '../../shared/services/weather-api/weather-api.service';
 import { InputAutocompleteComponent } from './components/input-autocomplete/input-autocomplete.component';
-import { map } from 'rxjs';
+import { map, tap } from 'rxjs';
 import { GeolocationInfoModel } from '../../shared/services/geocoding-api/geolocation-info.model';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { WeatherCardsListComponent } from './components/weather-cards-list/weather-cards-list.component';
 import { CurrentWeatherModel } from '../../shared/services/weather-api/current-weather.model';
+import { LocalStorageService } from '../../core/services/local-storage/local-storage.service';
 
 @Component({
   selector: 'app-weather-dashboard',
@@ -19,16 +20,22 @@ import { CurrentWeatherModel } from '../../shared/services/weather-api/current-w
   styleUrl: './weather-dashboard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WeatherDashboardComponent {
+export class WeatherDashboardComponent implements OnInit {
+
 
   dataOptionsCities = signal<GeolocationInfoModel[]>([]);
   currentCitiesWeather = signal<CurrentWeatherModel[]>([]);
 
   private geocodingService = inject(GeocodingAPIService);
   private weatherApiService = inject(WeatherApiService);
+  private localStorageService = inject(LocalStorageService);
   private destroyRef = inject(DestroyRef);
 
-  onInputChange(inputValue: string | null) {
+  ngOnInit(): void {
+    this.init();
+  }
+
+  public onInputChange(inputValue: string | null) {
     if (inputValue === null) {
       this.dataOptionsCities.set([]);
       return
@@ -42,17 +49,28 @@ export class WeatherDashboardComponent {
       .subscribe();
   }
 
-  onOptionSelect(cityData: GeolocationInfoModel) {
+  public onOptionSelect(cityData: GeolocationInfoModel) {
     this.weatherApiService.getCurrentWeather({ lat: cityData.lat, lon: cityData.lon })
       .pipe(
         map(val => this.currentCitiesWeather.update(curr => [val, ...curr])),
+        tap(() => this.localStorageService.setItem('currentCitiesWeatherList', JSON.stringify(this.currentCitiesWeather()))),
         takeUntilDestroyed(this.destroyRef)
       )
       .subscribe()
   }
 
-  onCardRemove(cardId: number): void {
+  public onCardRemove(cardId: number): void {
     this.currentCitiesWeather.update(
       (curr: CurrentWeatherModel[]): CurrentWeatherModel[] => curr.filter((card: CurrentWeatherModel) => card.id !== cardId))
+
+    this.localStorageService.setItem('currentCitiesWeatherList', JSON.stringify(this.currentCitiesWeather()));
+  }
+
+  private init() {
+    const storedCities = this.localStorageService.getItem('currentCitiesWeatherList');
+
+    if (storedCities) {
+      this.currentCitiesWeather.set(JSON.parse(storedCities));
+    }
   }
 }

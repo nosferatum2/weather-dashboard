b2351d5bcbf2415b8250e1cb32297fc4c9fe5d6d
nosferatum2 <artworkvo@gmail.com>
2024-12-26 14:15:36 +0200

    env api_key renamed

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,5 +4,5 @@ export const environment = {
   DOMAIN_URL: 'https://openweathermap.org',
   BASE_API_URL: "https://api.openweathermap.org",
 
-  APP_ID: "d9a0985de8cb79a9c27c212214fe390f",
+  API_KEY: "d9a0985de8cb79a9c27c212214fe390f",
 };
